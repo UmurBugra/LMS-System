67c04419e13fa25b649f4f48e5b9ed5771e603f9
UmurBugra <umurbugra61@gmail.com>
2025-04-28 19:23:32 +0300

    Giriş sonrası ana sayfaya yönlendirmek için eklemeler

--- routers/login_auth_routes.py
+++ routers/login_auth_routes.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, Form
-from schemas import LoginDisplay, LoginBase, AuthUserType, UserType
+from fastapi import APIRouter, Depends, Form, Request
+from schemas import LoginBase, AuthUserType, UserType
 from crud import auth
 from sqlalchemy.orm import Session
 from db.database import get_db
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+templates = Jinja2Templates(directory="templates")
 
 # User student or teacher
 @router.post("/")
@@ -19,15 +21,22 @@ def user_login(request: LoginBase, user_type: UserType ,db: Session = Depends(ge
 # Form verilerini işlemek için yeni endpoint
 # Arayüz giriş kısmında form verisi gönderiyordum fakat benden query bekliyordu bu şekilde form body den veriyi alıyor.
 @router.post("/login/form")
-def process_login(username: str = Form(...),
-                  password: str = Form(...),
-                  email: str = Form(...),
-                  user_type: UserType = Form(...),
-                  db: Session = Depends(get_db)):
+def process_login(request: Request,
+                username: str = Form(...),
+                password: str = Form(...),
+                email: str = Form(...),
+                user_type: UserType = Form(...),
+                db: Session = Depends(get_db)):
 
     login_data = LoginBase(username=username, password=password, email=email)
     result = auth.user_login(db, login_data, user_type)
     if result == "Giriş yapıldı":
-        return {"message": "Giriş başarılı"}
+        return templates.TemplateResponse(
+            "home.html",
+            {"request": request, "username": username, "email": email, "user_type": user_type}
+        )
     else:
-        return {"message": "Giriş başarısız. Hatalı bilgiler."}
+        return templates.TemplateResponse(
+            "index.html",
+            {"request": request, "error": result}
+        )
