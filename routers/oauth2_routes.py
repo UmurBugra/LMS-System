fdc35dff292c9cb76a9731be37ba0489136d1494
UmurBugra <umurbugra61@gmail.com>
2025-06-27 22:33:59 +0300

    Kullanıcı doğrulama işlemlerinde kullanıcı adı yerine e-posta kullanılarak kullanıcı bilgileri alındı; erişim token'ı oluşturulurken kullanıcı adı güncellendi.

--- routers/oauth2_routes.py
+++ routers/oauth2_routes.py
@@ -11,7 +11,7 @@ router = APIRouter(
 )
 @router.post("/token") # tokenUrl ile aynı olmalı.
 def get_token(request: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db)):
-    user = db.query(LoginData).filter(LoginData.username == request.username).first()
+    user = db.query(LoginData).filter(LoginData.email == request.username).first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -23,9 +23,9 @@ def get_token(request: OAuth2PasswordRequestForm = Depends(),db: Session = Depen
             detail="Hatalı giriş bilgileri. Lütfen tekrar deneyin.",
         )
 
-    access_token = oauth2.create_access_token(data={"sub": request.username})
+    access_token = oauth2.create_access_token(data={"sub": user.username})
     return {
         "access_token": access_token,
         "token_type": "bearer",
-        "username": request.username,
+        "username": user.username,
     }
