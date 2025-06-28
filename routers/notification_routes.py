556f7f90103e7347e0721be855b2309708cb35d4
UmurBugra <umurbugra61@gmail.com>
2025-06-28 23:24:14 +0300

    get_notifications_route fonksiyonuna current_user.id parametresi eklendi

--- routers/notification_routes.py
+++ routers/notification_routes.py
@@ -12,7 +12,7 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 def get_notifications_route(request: Request, db: Session = Depends(get_db), current_user: LoginBase = Depends(get_current_user_from_cookie)):
-    notifications = get_notifications(db, current_user.username)
+    notifications = get_notifications(db, current_user.username, current_user.id)
     return templates.TemplateResponse("notifications.html", {"request": request, "notifications": notifications})
 
 @router.post("/create")
