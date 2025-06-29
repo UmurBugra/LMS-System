d4ba7955014ce7640c0f84779ab0624e7c20e6bf
UmurBugra <umurbugra61@gmail.com>
2025-06-29 13:21:44 +0300

    takvim görüntülemesi için get_user_student fonksiyonu eklendi

--- crud/login.py
+++ crud/login.py
@@ -57,3 +57,9 @@ def get_user_by_email_and_id(db: Session, email: str, user_id: int):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+def get_user_student(db: Session, email: str, user_id: int, user_type: UserType):
+    user = db.query(LoginData).filter(LoginData.email == email, LoginData.id == user_id, LoginData.type == user_type).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found or not a student")
+    return user
