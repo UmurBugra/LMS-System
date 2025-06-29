a3f5277537781827a2f1187fcc1efb399331f13d
UmurBugra <umurbugra61@gmail.com>
2025-06-29 18:27:36 +0300

    setup post endpointini form verisi alacak şekilde güncellendi

--- routers/setup_routes.py
+++ routers/setup_routes.py
@@ -1,13 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
 from db.database import get_db
 from crud import setup
-from schemas import LoginBase
+from schemas import LoginBase,UserType
 
 router = APIRouter(prefix="/setup", tags=["setup"])
 templates = Jinja2Templates(directory="templates")
 
 @router.post("/")
-def create_setup(request: LoginBase, db: Session = Depends(get_db)):
-    return setup.create_setup_user(db, request)
+def create_setup(
+        username: str = Form(...),
+        password: str = Form(...),
+        email: str = Form(...),
+        db: Session = Depends(get_db)
+):
+    request = LoginBase(username=username, password=password, email=email)
+    return setup.create_setup_user(db, request, user_type=UserType.admin)
