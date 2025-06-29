41dc892fa690442c38fc198da52df01059fc1c24
UmurBugra <umurbugra61@gmail.com>
2025-06-29 21:42:00 +0300

    Kullanıcı güncelleme endpointi Body ile veri alacak şekilde düzenlendi, silme endpointinden authentication kaldırıldı

--- routers/admin_routes.py
+++ routers/admin_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, Query, Form
+from fastapi import APIRouter, Depends, Query, Form, Body
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
 from db.database import get_db
 from schemas import LoginBase,UserType
 from crud import admin
-from authentication.oauth2 import create_user_authentication_token, admin_authentication_token
+from authentication.oauth2 import admin_authentication_token
 from schemas import LoginDisplay
 
 router = APIRouter(prefix="/admin-page", tags=["setup"])
@@ -22,22 +22,26 @@ def create_user(
     request = LoginBase(username=username, email=email, password=password)
     return admin.create_user_by_admin(db, request, type)
 
-# Read all users
-@router.get("/get-user-all", response_model=list[LoginDisplay])
-def get_users(db: Session = Depends(get_db)):
-    return admin.get_read_users_by_admin(db)
-
-# Read user by id
-@router.get("/get-user-by-{id}", response_model=LoginDisplay)
-def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    return admin.read_user_by_admin(db, id)
+# # Read all users
+# @router.get("/get-user-all", response_model=list[LoginDisplay])
+# def get_users(db: Session = Depends(get_db)):
+#     return admin.get_read_users_by_admin(db)
 
 # Update user
 @router.put("/update-user-{id}", response_model=LoginDisplay)
-def update_user(id: int, request: LoginBase, db: Session = Depends(get_db), type: UserType = Query(...)):
-    return admin.update_user_by_admin(db, id, request, type)
+def update_user(
+    id: int,
+    data: dict = Body(...),
+    db: Session = Depends(get_db)
+):
+    request = LoginBase(
+        username=data["username"],
+        email=data["email"],
+        password=data["password"]
+    )
+    return admin.update_user_by_admin(db, id, request, data["type"])
 
 # Delete user
 @router.delete("/delete-user-{id}")
-def delete_user(id: int, db: Session = Depends(get_db), delete_user_by_auth: LoginBase = Depends(create_user_authentication_token)):
+def delete_user(id: int, db: Session = Depends(get_db)):
     return admin.delete_user_by_admin(db, id)
