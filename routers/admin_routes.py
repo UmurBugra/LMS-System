70767982de66eb416ceea0bf501277628c232bab
UmurBugra <umurbugra61@gmail.com>
2025-06-29 19:24:53 +0300

    admin için API

---
+++ routers/admin_routes.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
+from db.database import get_db
+from schemas import LoginBase,UserType
+from crud import admin
+from authentication.oauth2 import create_user_authentication_token
+from schemas import LoginDisplay
+
+router = APIRouter(prefix="/admin-page", tags=["setup"])
+templates = Jinja2Templates(directory="templates")
+
+@router.post("/create", response_model=LoginDisplay)
+def create_user(request: LoginBase, db: Session = Depends(get_db), type: UserType = Query(...),
+                create_user_by_auth: LoginBase = Depends(create_user_authentication_token)):
+    return admin.create_user_by_admin(db, request, type)
+
+# Read all users
+@router.get("/get-user-all", response_model=list[LoginDisplay])
+def get_users(db: Session = Depends(get_db)):
+    return admin.get_read_users_by_admin(db)
+
+# Read user by id
+@router.get("/get-user-by-{id}", response_model=LoginDisplay)
+def get_user_by_id(id: int, db: Session = Depends(get_db)):
+    return admin.read_user_by_admin(db, id)
+
+# Update user
+@router.put("/update-user-{id}", response_model=LoginDisplay)
+def update_user(id: int, request: LoginBase, db: Session = Depends(get_db), type: UserType = Query(...)):
+    return admin.update_user_by_admin(db, id, request, type)
+
+# Delete user
+@router.delete("/delete-user-{id}")
+def delete_user(id: int, db: Session = Depends(get_db), delete_user_by_auth: LoginBase = Depends(create_user_authentication_token)):
+    return admin.delete_user_by_admin(db, id)
