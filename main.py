d2f0d7047b4b440c44e4f3249b0abe88132cb7f5
UmurBugra <umurbugra61@gmail.com>
2025-06-30 15:55:43 +0300

    kod temizliği

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, Depends
-from routers import login_routes, login_auth_routes, calendar_routes, oauth2_routes, navigation_routes, \
+from routers import login_auth_routes, calendar_routes, oauth2_routes, navigation_routes, \
 notification_routes, setup_routes, admin_routes
 from db.database import engine, get_db
 from db import models
@@ -12,7 +12,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates") # Jinja2 şablonları için dizin
 
-app.include_router(login_routes.router)
 app.include_router(login_auth_routes.router)
 app.include_router(calendar_routes.router)
 app.include_router(oauth2_routes.router)
