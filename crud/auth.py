cdfecadc1af9679410c0e3a52bef4c9ae2d4f9ce
UmurBugra <umurbugra61@gmail.com>
2025-06-30 15:54:52 +0300

    kod temizliği

--- crud/auth.py
+++ crud/auth.py
@@ -1,26 +1,8 @@
 from sqlalchemy.orm import Session
-from schemas import LoginBase, AuthUserType, UserType, LoginEmailPassword
+from schemas import LoginEmailPassword
 from db.models import LoginData
-from fastapi import HTTPException, status
 
 # CRUD operations for user authentication
-
-# User student or teacher
-def user_type_request(db: Session, request: AuthUserType):
-    user = db.query(LoginData).filter(
-        LoginData.username == request.username, LoginData.email == request.email).first()
-
-    if not user:
-        return "User not found"
-
-    user_type = user.type.value
-    if user_type == "student":
-        return "Öğrenci"
-    elif user_type == "teacher":
-        return "Öğretmen"
-    else:
-        return "Unknown user type"
-
 # User Login Control
 def user_login(db: Session, request: LoginEmailPassword): # Parametreler güncellendi
     user = db.query(LoginData).filter(LoginData.email == request.email).first()
@@ -29,7 +11,6 @@ def user_login(db: Session, request: LoginEmailPassword): # Parametreler güncel
 
     if user.password != request.password:
         return "Hatalı giriş bilgileri. Lütfen tekrar deneyin."
-        # Başarılı giriş durumunda kullanıcı bilgilerini döndür
     return {
         "message": "Giriş yapıldı",
         "username": user.username,
