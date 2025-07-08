cc22cbc6b598a12d43db797cfca1b7f423d6349b
UmurBugra <umurbugra61@gmail.com>
2025-07-08 13:42:38 +0300

    sqlite urlsi PostgreSQL ile değişti

--- db/database.py
+++ db/database.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///databases.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://db_admin:admin1234@localhost:5432/databases"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
