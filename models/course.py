912868fba47588f31ddcbecfa07f2312cd1d776c
UmurBugra <umurbugra61@gmail.com>
2025-07-17 16:04:44 +0300

    kurs modeli

---
+++ models/course.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import relationship
+from db.database import Base
+
+
+class Course(Base):
+    __tablename__ = "courses"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    code = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+    # Oluşturulan öğretmen
+    teacher_id = Column(Integer, ForeignKey("login_data.id"), nullable=False)
+    teacher = relationship("LoginData", back_populates="created_courses")
+
+    event = relationship("Event", back_populates="course")
+    enrollments = relationship("Enrollment", back_populates="course")
