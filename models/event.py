7d4e8849b4bb86e9561ee04d97522639d8b7ebb8
UmurBugra <umurbugra61@gmail.com>
2025-07-17 16:04:58 +0300

    event modeli eklendi ve alanlar güncellendi

--- models/event.py
+++ models/event.py
@@ -1,20 +1,20 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy import Enum as SQLAlchemyEnum
 from db.database import Base
-from schemas import CalendarBase
+from schemas import EventType, EventStatus
 
-class CalendarData(Base):
-    __tablename__ = "calendar_data"
+
+class Event(Base):
+    __tablename__ = "events"
     id = Column(Integer, primary_key=True, index=True)
-    days = Column(SQLAlchemyEnum(CalendarBase), unique=False)
-    t_08_09 = Column(String)
-    t_09_10 = Column(String)
-    t_10_11 = Column(String)
-    t_11_12 = Column(String)
-    t_13_14 = Column(String)
-    t_14_15 = Column(String)
-    t_15_16 = Column(String)
-    t_16_17 = Column(String)
-    user_id = Column(Integer, ForeignKey("login_data.id"))
-    user = relationship("LoginData", back_populates="items")
+    title = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+    start_time = Column(String, nullable=False)
+    end_time = Column(String, nullable=False)
+    location = Column(String, nullable=True)    # --> amfi veya online
+    event_type = Column(SQLAlchemyEnum(EventType))
+    event_status = Column(SQLAlchemyEnum(EventStatus))
+    # kurs kime ait
+    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+    course = relationship("Course", back_populates="event")
