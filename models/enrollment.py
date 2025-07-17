a51c887396ddbb5f8e0776af24cdf2de70bad842
UmurBugra <umurbugra61@gmail.com>
2025-07-17 16:04:50 +0300

    enrollment modeli eklendi

---
+++ models/enrollment.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy import ForeignKey
+from db.database import Base
+from sqlalchemy.orm import relationship
+
+class Enrollment(Base):
+    __tablename__ = "enrollments"
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey("login_data.id"), nullable=False)
+    course_id = Column(Integer, ForeignKey("courses.id"), nullable=False)
+
+    # Many-to-many relationship
+    student = relationship("LoginData", back_populates="enrollments")
+    course = relationship("Course", back_populates="enrollments")
+
