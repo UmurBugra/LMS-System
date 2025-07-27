0e0a2681867ebb2e19c8a5ecf7ee70c41eb35772
UmurBugra <umurbugra61@gmail.com>
2025-07-27 22:39:45 +0300

    enrollment için oluşturma ve silme fonksiyonları eklendi

---
+++ crud/enrollment.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from models import Enrollment, Course, LoginData
+
+def create_enrollment(db: Session, course_id: int, student_id: int):
+
+    new_enrollment = Enrollment(
+        course_id=course_id,
+        student_id=student_id
+    )
+    db.add(new_enrollment)
+    db.commit()
+    db.refresh(new_enrollment)
+
+    return new_enrollment
+
+def delete_enrollment(db: Session, course_id: int, student_id: int):
+    enrollment = db.query(Enrollment).filter(
+        Enrollment.course_id == course_id,
+        Enrollment.student_id == student_id
+    ).first()
+
+    if enrollment:
+        db.delete(enrollment)
+        db.commit()
+
+    return enrollment
