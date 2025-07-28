a24e8d685185fa685241d92a31701f83b78e8a82
UmurBugra <umurbugra61@gmail.com>
2025-07-28 15:12:32 +0300

    course için listeleme fonksiyonu eklendi

--- crud/course.py
+++ crud/course.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
-from models import Course, Enrollment, LoginData
-from schemas import UserType
+from models import Course, Enrollment,LoginData
 
+# Kurs oluşturma CRUD
 def create_course(db: Session, name: str, code: str, description: str, current_user):
 
     new_course = Course(
@@ -15,3 +15,23 @@ def create_course(db: Session, name: str, code: str, description: str, current_u
     db.refresh(new_course)
 
     return new_course
+
+# Kurs listeleme CRUD
+
+def get_courses(db, student_id):
+
+    get_c = db.query(Course, Enrollment).join(
+        LoginData,
+        LoginData.id == Enrollment.student_id
+    ).filter(
+        Course.id == Enrollment.course_id,
+        Enrollment.student_id == student_id
+    )
+
+    courses = []
+
+    for i in get_c:
+
+        courses.append(i)
+
+    return courses
